4fce4919272dede528cfc9930bbcdcaef7170e1b
bai <1612348548@qq.com>
2020-03-23 15:59:20 +0800

    登录页面结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入element包
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入路由实例
+import router from './router/index.js'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login-container">
+    <el-card class="my-card">
+      <img src="../assets/logo_index.png" alt />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-login"
+};
+</script>
+
+<style lang='less'>
+.login-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: url(../assets/login_bg.jpg) no-repeat center/cover;
+  .my-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
