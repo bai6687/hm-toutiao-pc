f6925267463b4bd394c88562296f874103366a3f
bai <1612348548@qq.com>
2020-03-24 10:48:00 +0800

    登录模块-自定义校验规则

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,7 +4,7 @@
       <img src="../assets/logo_index.png" alt />
       <!-- 表单 -->
       <el-form :model="loginForm" :rules="loginRules">
-        <el-form-item prop="mobile">
+        <el-form-item prop="mobile" status-icon>
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <el-form-item prop="code">
@@ -30,6 +30,19 @@
 export default {
   name: "my-login",
   data() {
+    // 自定义校验函数--先定义后使用
+    // rule：当前字段校验规则对象，一般不使用
+    // value：当前字段对应数据
+    // callback：
+    const checkMobile = (rule, value, callback) => {
+      // 检验value的值，必须符合手机格式
+      // 格式：1 开头，第二个数组 3-9之间,9个数字结尾
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback();
+      } else {
+        callback(new Error("手机号格式不对"));
+      }
+    };
     return {
       loginForm: {
         mobile: "",
@@ -37,7 +50,9 @@ export default {
       },
       loginRules: {
         mobile: [
-          { required: true, message: "请输入手机号码", trigger: "blur" }
+          { required: true, message: "请输入手机号码", trigger: "blur" },
+          // 使用validator指定一个自定义校验函数
+          { validator: checkMobile, trigger: "blur" }
         ],
         code: [
           { required: true, message: "请输入验证码", trigger: "blur" },
